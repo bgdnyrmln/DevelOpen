244b204dd49d9d855be5205987127da1dd1ff001
bgdnyrmln <bgdnyrmln@gmail.com>
2024-10-17 18:32:38 +0300

    qwe
    qwe

--- pages/script.js
+++ pages/script.js
@@ -1,23 +1,3 @@
-function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('cards-show');
-      }
-    });
-  }
-  
-
-
-  let options = {
-    threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.cards-animation');
-  
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
-
-
   const headerline = document.getElementById("headerline");
   const viewportHeight = window.innerHeight;
   const scrollLimit = 0.99 * viewportHeight;
